95e4a8a4f8ed609a7ba09fb8f5e0dc4b9d930c6f
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-10 09:34:27 -0300

    adic. store

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    recipe:[
+      {
+        title: 'Pé de Moleque',
+        slug: 'pe-de-moleque',
+        description: 'Doce tradicional nordestino',
+        ingredients: ['1/2 kg de amendoim torrado e descascado', '1/2 kg de açúcar', '1 lata de leite condensado', '3 colheres de margarina'],
+        methods: ['Colocar na panela o amendoim, o açúcar e a margarina', 'Levar ao fogo, mexendo sempre', 'Quando começar a formar uma calda, coloque o leite condensado', 'Mexa bem, até soltar do fundo da panela, como brigadeiro', 'Coloque no tabuleiro untado com margarina', 'Deixe esfriar e corte em pedaços']
+      }
+    ]
   },
   getters: {
   },
