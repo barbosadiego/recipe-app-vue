8affd04ec930ec15e289d074234fbf96414cb58a
diegobarbosa <diego.gutemberg@hotmail.com>
2022-08-13 11:19:03 -0300

    remove console-logs

--- src/store/index.js
+++ src/store/index.js
@@ -18,19 +18,16 @@ export default new Vuex.Store({
     },
     START(state){
       const data = JSON.parse(localStorage.getItem('recipes'));
-      console.log(data)
       if(data) {
         try{
           state.recipe.push( ...data );
         } catch(e){
           localStorage.removeItem('recipes');
-          console.log(e);
         }
       } 
     },
     SHOW_ADD_RECIPE_FORM(state){
       state.isActive = !state.isActive;
-      console.log(state.isActive)
     }
   },
   actions: {
